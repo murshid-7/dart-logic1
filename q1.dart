6b680915e2abc0c409b89315e9e0c4b0b9ecd178
murshid-7 <147391448+murshid-7@users.noreply.github.com>
2024-04-05 09:53:18 +0530

    commited

--- q1.dart
+++ q1.dart
@@ -1,6 +1,6 @@
 void main() {
   //Q- Given a string s consisting of words and spaces, return the length of the last word in the string.
-  String q1 = "   fly me   to   the moon  ";
+  String q1 = "   fly me   q46y4hu5to   the moon  ";
   print(lengthOfWord(q1));
 }
 
